860bb0efcc907e36537ce0e6673db68055394cf2
Jikku Jose <jikkujose@gmail.com>
2017-10-13 20:17:07 +0530

    Update coins

--- src/data/pairs.js
+++ src/data/pairs.js
@@ -1,11 +1,12 @@
 const pairs = [
+  { from: "BTC", to: "USD" },
   { from: "BTC", to: "ETH" },
   { from: "BTC", to: "DOGE" },
   { from: "BTC", to: "LTC" },
   { from: "BTC", to: "ETC" },
   { from: "BTC", to: "BCH" },
   { from: "ETH", to: "ETC" },
-  { from: "ETH", to: "ERROR" },
+  { from: "ETH", to: "USD" },
 ]
 
 export default pairs
