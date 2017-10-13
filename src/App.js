e4aa5b6f5ccd584e4eb777bd1058bd2f9f81ad94
Jikku Jose <jikkujose@gmail.com>
2017-10-13 09:29:28 +0530

    Add cryptos

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ const pairs = [
   { from: "BTC", to: "DOGE" },
   { from: "BTC", to: "LTC" },
   { from: "BTC", to: "ETC" },
+  { from: "BTC", to: "BCH" },
+  { from: "ETH", to: "ETC" },
 ]
 
 export default function App() {
