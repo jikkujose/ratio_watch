1973e1f0a86c0bdbe90ba605fd3e6c8d20d6a7a9
Jikku Jose <jikkujose@gmail.com>
2017-10-16 20:33:08 +0530

    Add Service worker

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 import "tachyons"
+import registerServiceWorker from "./registerServiceWorker"
 
 ReactDOM.render(<App />, document.getElementById("root"))
+registerServiceWorker()
