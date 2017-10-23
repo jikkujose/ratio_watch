4a37370451bc144c95d650dcf2a85c6d0dfba492
Jikku Jose <jikkujose@gmail.com>
2017-10-23 09:27:58 +0530

    Update pairs

--- src/data/pairs.js
+++ src/data/pairs.js
@@ -1,10 +1,12 @@
 const pairs = [
   { from: "BTC", to: "USD" },
+  { from: "ETH", to: "USD" },
+  { from: "BCH", to: "USD" },
   { from: "BTC", to: "ETH" },
-  { from: "BTC", to: "DOGE" },
   { from: "BTC", to: "LTC" },
   { from: "BTC", to: "ETC" },
-  { from: "BTC", to: "BCH" },
+  { from: "ETC", to: "NMR" },
+  { from: "ETC", to: "DOGE" },
 ]
 
 export default pairs
