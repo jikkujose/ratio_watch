ce7724bb8ad4c4549f74300d3181b1cc64defa21
Jikku Jose <jikkujose@gmail.com>
2017-11-06 10:07:58 +0530

    Update pairs

--- src/data/pairs.js
+++ src/data/pairs.js
@@ -1,9 +1,10 @@
 const pairs = [
   { from: "BTC", to: "USD" },
   { from: "ETH", to: "USD" },
+  { from: "ETC", to: "USD" },
   { from: "BCH", to: "USD" },
+  { from: "ETH", to: "ETC" },
   { from: "BTC", to: "ETH" },
-  { from: "BTC", to: "LTC" },
   { from: "BTC", to: "ETC" },
   { from: "ETC", to: "NMR" },
   { from: "ETC", to: "DOGE" },
