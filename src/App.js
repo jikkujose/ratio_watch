c3acce51367a9dbf60f1e46975c7b9c8baf840b5
Jikku Jose <jikkujose@gmail.com>
2017-11-12 16:42:36 +0530

    Update with more currencies & clean UI

--- src/App.js
+++ src/App.js
@@ -6,6 +6,46 @@ import { ShapeShift, CryptoCompare } from "utils"
 
 import { pairs } from "data"
 
+export default class App extends React.Component {
+  state = {
+    pairs,
+    viewMode: true,
+    APIs: { ss: ShapeShift, cc: CryptoCompare },
+  }
+
+  toggleMode = () => this.setState(s => ({ viewMode: !s.viewMode }))
+
+  addPair = (from, to) => {
+    this.setState(s => ({
+      pairs: s.pairs.concat({ from, to, api: "cc" }),
+      viewMode: true,
+    }))
+  }
+
+  render() {
+    const { APIs, pairs, viewMode } = this.state
+    const showControls = true
+
+    return (
+      <Wrapper>
+        <Container>
+          {viewMode
+            ? pairs.map((pair, i) =>
+                <RatePanel key={i} {...pair} API={APIs[pair.api]} />
+              )
+            : <AddForm handleSubmit={this.addPair} />}
+          <Footer>
+            {showControls &&
+              <Link onClick={this.toggleMode}>
+                {viewMode ? "Add" : "Cancel"}
+              </Link>}
+          </Footer>
+        </Container>
+      </Wrapper>
+    )
+  }
+}
+
 const Wrapper = styled.div.attrs({
   className: "flex justify-center",
 })``
@@ -16,10 +56,6 @@ const Container = styled.div.attrs({
   max-width: 960px;
 `
 
-const Header = styled.div.attrs({
-  className: "pa3 ph0-ns code f4",
-})``
-
 const Link = styled.a.attrs({
   className: "link b pointer",
 })``
@@ -27,62 +63,3 @@ const Link = styled.a.attrs({
 const Footer = styled.div.attrs({
   className: "f6 code pa3 ph0-ns flex justify-between",
 })``
-
-const twitterProfile = "https://twitter.com/big_org"
-
-export default class App extends React.Component {
-  state = {
-    pairs,
-    viewMode: true,
-    APIs: [ShapeShift, CryptoCompare],
-    selectedAPIIndex: 1,
-  }
-
-  switchAPIs = () => {
-    const { APIs, selectedAPIIndex } = this.state
-    const count = APIs.length
-
-    const nextIndex = (selectedAPIIndex + 1) % count
-
-    this.setState(s => ({ selectedAPIIndex: nextIndex }))
-  }
-
-  toggleMode = () => this.setState(s => ({ viewMode: !s.viewMode }))
-
-  addPair = (from, to) => {
-    this.setState(s => ({
-      pairs: s.pairs.concat({ from, to }),
-      viewMode: true,
-    }))
-  }
-
-  render() {
-    const { APIs, selectedAPIIndex, pairs, viewMode } = this.state
-    const API = APIs[selectedAPIIndex]
-    const showControls = true
-    console.log(API.name)
-
-    return (
-      <Wrapper>
-        <Container>
-          <Header>Rate Watch</Header>
-          {viewMode
-            ? pairs.map((pair, i) => <RatePanel key={i} {...pair} API={API} />)
-            : <AddForm handleSubmit={this.addPair} />}
-          <Footer>
-            <div>
-              <Link href={twitterProfile}>BIGOrg</Link> | API: {API.name}
-            </div>
-            {showControls &&
-              <div>
-                {false && <Link onClick={this.switchAPIs}>Switch API</Link>}
-                <Link onClick={this.toggleMode}>
-                  {viewMode ? "Add" : "Cancel"}
-                </Link>
-              </div>}
-          </Footer>
-        </Container>
-      </Wrapper>
-    )
-  }
-}
--- src/data/pairs.js
+++ src/data/pairs.js
@@ -1,13 +1,15 @@
 const pairs = [
-  { from: "BTC", to: "USD" },
-  { from: "ETH", to: "USD" },
-  { from: "ETC", to: "USD" },
-  { from: "BCH", to: "USD" },
-  { from: "ETH", to: "ETC" },
-  { from: "BTC", to: "ETH" },
-  { from: "BTC", to: "ETC" },
-  { from: "ETC", to: "NMR" },
-  { from: "ETC", to: "DOGE" },
+  { from: "BTC", to: "USD", api: "cc" },
+  { from: "ETH", to: "USD", api: "cc" },
+  { from: "ETC", to: "USD", api: "cc" },
+  { from: "BCH", to: "USD", api: "cc" },
+  { from: "ETH", to: "ETC", api: "ss" },
+  { from: "BTC", to: "ETH", api: "ss" },
+  { from: "BTC", to: "BCH", api: "ss" },
+  { from: "BTC", to: "ETC", api: "ss" },
+  { from: "ETC", to: "DOGE", api: "ss" },
+  { from: "ETC", to: "NMR", api: "ss" },
+  { from: "ZEC", to: "ETC", api: "ss" },
 ]
 
 export default pairs
